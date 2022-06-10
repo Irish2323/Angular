ab9a5aec04de3bc118ebbd26ca23a99f2bb1ccca
baijeehan <idelosreyes2323@gmail.com>
2022-06-10 22:08:44 +0800

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+import { Environment } from '@abp/ng.core';
+
+const baseUrl = 'https://delosreyes-bookstore.netlify.app';
+
+export const environment = {
+  production: true,
+  application: {
+    baseUrl,
+    name: 'BookStore',
+    logoUrl: '',
+  },
+  oAuthConfig: {
+    issuer: 'http://reyes23-001-site1.etempurl.com',
+    redirectUri: baseUrl,
+    clientId: 'BookStore_App',
+    responseType: 'code',
+    scope: 'offline_access BookStore',
+    requireHttps: false,
+  },
+  apis: {
+    default: {
+      url: 'http://reyes23-001-site1.etempurl.com',
+      rootNamespace: 'Acme.BookStore',
+    },
+  },
+} as Environment;
